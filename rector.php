67b5a9f4a08e983a0b3c49a07464225f8ba8ef8e
Patricio <ijpatricio@users.noreply.github.com>
2026-01-08 06:44:13 +0000

    Fix `strictBooleans` deprecated.
    Fixes the warning I got after installation and running `composer refactor`
    
    ```
    [WARNING] The "strictBooleans" set is deprecated as mostly risky and not practical. Remove it from withPreparedSets()  
               method and use "codeQuality" and "codingStyle" sets instead. They already contain more granular and stable   
               rules on same note.          
    ```

--- rector.php
+++ rector.php
@@ -19,6 +19,6 @@ return RectorConfig::configure()
         typeDeclarations: true,
         privatization: true,
         earlyReturn: true,
-        strictBooleans: true,
+        codingStyle: true,
     )
     ->withPhpSets();
